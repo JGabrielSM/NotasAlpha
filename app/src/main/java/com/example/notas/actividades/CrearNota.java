650b27bf8777da1ed73561d3a21ff406a5992fe1
damon <j.gabriel.sanchez.mavalencia@gmail.com>
2020-10-05 13:24:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/notas/actividades/CrearNota.java
@@ -0,0 +1,16 @@
+package com.example.notas.actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.notas.R;
+
+public class CrearNota extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_nota);
+    }
+}
---
+++ app/src/main/java/com/example/notas/clases/nota.java
@@ -0,0 +1,41 @@
+package com.example.notas.clases;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class nota implements Parcelable {
+
+    private String titulo;
+    private String contenido;
+
+    public nota(String titulo, String contenido) {
+        this.titulo = titulo;
+        this.contenido = contenido;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+    }
+}
